3e869599a4fafdb52126958eef4ecca1324a71fb
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-16 22:26:10 -0500

    Added DB integration

--- flaskr/app.py
+++ flaskr/app.py
@@ -10,12 +10,20 @@ from routes.web import web_bp
 app = Flask(__name__)
 
 # Configuración básica de la aplicación
-app.config['SECRET_KEY'] = 'magneto'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+db = SQLAlchemy()
+
+def setup_app():
+    app.config['SECRET_KEY'] = 'magneto'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return app
+
+
 
 # Inicialización de SQLAlchemy
-db = SQLAlchemy(app)
 
 # Configuración de Flask-Login
 
