aae53c0848acfda3c6c1845cafffcdae4885eef7
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-17 00:06:59 -0500

    Added DB to web

--- flaskr/routes/auth.py
+++ flaskr/routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import request, redirect, url_for, render_template
-from models import db, CompanyAdmin
+from models import CompanyAdmin
+from models.database import db
 
 import os
 
@@ -15,29 +16,4 @@ from controllers import data_controller as data
 
 auth_bp = Blueprint('auth', __name__)
 
-# Define las rutas y las funciones de las vistas aquí
 
-@auth_bp.route("/main", endpoint="index")
-
-@auth_bp.route("/register_company", methods=['POST'])
-def register_company():
-    print(request.form)
-    company_name = request.form['company_name']
-    business_name = request.form['business_name']
-    main_email = request.form['main_email']
-    person_in_charge = request.form['person_in_charge']
-    role_in_company = request.form['role_in_company']
-    one_time_password = request.form['one_time_password']
-
-    new_company = CompanyAdmin(
-        company_name=company_name,
-        business_name=business_name,
-        main_email=main_email,
-        person_in_charge=person_in_charge,
-        role_in_company=role_in_company,
-        one_time_password=one_time_password
-    )
-    db.session.add(new_company)
-    db.session.commit()
-
-    return redirect(url_for('some_page')) 
--- flaskr/routes/web.py
+++ flaskr/routes/web.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
+from flask import request, redirect, url_for, render_template
+from models import CompanyAdmin
+from models.database import db
+
 
 import os
 
@@ -39,4 +43,26 @@ def register_response_route():
 
 @web_bp.route("/forgot_password", endpoint="forgot_password")
 def profile_route():
-    return user.forgot_password()
+
+@web_bp.route("/register_company", methods=['POST'])
+def register_company():
+    print(request.form)
+    company_name = request.form['company_name']
+    business_name = request.form['business_name']
+    main_email = request.form['main_email']
+    person_in_charge = request.form['person_in_charge']
+    role_in_company = request.form['role_in_company']
+    one_time_password = request.form['one_time_password']
+
+    new_company = CompanyAdmin(
+        company_name=company_name,
+        business_name=business_name,
+        main_email=main_email,
+        person_in_charge=person_in_charge,
+        role_in_company=role_in_company,
+        one_time_password=one_time_password
+    )
+    db.session.add(new_company)
+    db.session.commit()
+
+    return redirect(url_for('some_page')) 
