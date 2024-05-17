f1d486c95defaad9860a586b400fccb3a56a6d37
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-17 05:33:03 -0500

    Trying DB

--- flaskr/models/Employee.py
+++ flaskr/models/Employee.py
@@ -1,9 +1,11 @@
 
 from flask_login import UserMixin
+from sqlalchemy.schema import ForeignKeyConstraint
 from models.database import db
 from models.CompanyAdmin import CompanyAdmin
 
 class Employee(db.Model):
+    __tablename__ = 'employee'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
