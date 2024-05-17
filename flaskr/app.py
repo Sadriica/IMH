fbf1a3d70564e96b339f260ad158e7f7ddd3c03d
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-17 02:35:28 -0500

    Fixing DB

--- flaskr/app.py
+++ flaskr/app.py
@@ -12,7 +12,7 @@ from routes.web import web_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASE_DIR}/app.db'  
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'magneto'
 
