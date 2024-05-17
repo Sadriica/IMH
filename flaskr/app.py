c89cd4f7ee916c6e71fcdb555e98b4dea0b0453e
Sadriicaa <santirodrigmoca@outlook.com>
2024-05-17 01:48:05 -0500

    Added file

--- flaskr/app.py
+++ flaskr/app.py
@@ -14,6 +14,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'magneto'
 
     db.init_app(app)
     app.register_blueprint(web_bp)
